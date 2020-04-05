af653cf2f80bc4c3e71f0c98e55769e582520ec2
sturfee-petrl <peterl@sturfee.com>
2020-04-05 15:39:55 -0700

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 : "${local_proxySearchEngine_HOST?Need to set local_proxySearchEngine_HOST what is the domain of the proxy}"
 : "${local_proxySearchEngine_LETSENCRYPT_EMAIL?Need to set local_proxySearchEngine_LETSENCRYPT_EMAIL what is the email for lets encrypt}"
 
-PORT="$local_proxySearchEngine_PORT"
-HOST="$local_proxySearchEngine_HOST"
-VIRTUAL_HOST="$local_proxySearchEngine_HOST"
-LETSENCRYPT_HOST="$local_proxySearchEngine_HOST"
-LETSENCRYPT_EMAIL="$local_proxySearchEngine_LETSENCRYPT_EMAIL"
+export PORT="$local_proxySearchEngine_PORT"
+export HOST="$local_proxySearchEngine_HOST"
+export VIRTUAL_HOST="$HOST"
+export LETSENCRYPT_HOST="$HOST"
+export LETSENCRYPT_EMAIL="$local_proxySearchEngine_LETSENCRYPT_EMAIL"
 
 docker-compose up --build -d
